cad51b11f0642bddd41f72b68bb94f1b5043e1c7
ruben <ruben@DESKTOP-JO9KI7I>
2020-02-29 20:13:44 +0100

    Utliza las funciones de refactorización para cambiar la cabecera del método imprimir. Le cambiarás el nombre por imprime.

--- src/tarea4ed_rubéngarcíacaparrós/TiradaMonedaCX.java
+++ src/tarea4ed_rubéngarcíacaparrós/TiradaMonedaCX.java
@@ -78,10 +78,10 @@ public class TiradaMonedaCX {
             }
         }
 
-        Imprimir(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
+        Imprime(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
     }
 
-    public static void Imprimir(int contadorCaras, int contadorCruces, int maximoCarasDoblesConsecutivas, int maximoCrucesDoblesConsecutivas) {
+    public static void Imprime(int contadorCaras, int contadorCruces, int maximoCarasDoblesConsecutivas, int maximoCrucesDoblesConsecutivas) {
         System.out.println("Caras: " + contadorCaras + ", Cruces: " + contadorCruces);
         System.out.println("Maximo de caras dobles consecutivas: " + maximoCarasDoblesConsecutivas);
         System.out.println("Maximo de cruces dobles consecutivas: " + maximoCrucesDoblesConsecutivas);
